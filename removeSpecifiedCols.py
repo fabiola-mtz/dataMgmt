55501ba583d757396d3fb1a8c0ed0cfaa5993798
Martinez Ortiz <fabiola.martinez.ortiz@sap.com>
2024-02-21 16:04:40 -0600

    u- my.py: 1st upgraded verision

--- removeSpecifiedCols.py
+++ removeSpecifiedCols.py
@@ -8,9 +8,8 @@ class removeSpecifiedCols:
     def __init__(self, CSVpath, saveCSVpath):
         self.CSVpath = CSVpath
         self.saveCSVpath = saveCSVpath
-                
-    def remove_cols(self):
-        ext = ('.csv')
+    
+    def create_path(self, ext):
         # Lists all files within given path
         for filename in os.listdir(self.CSVpath):
             # Concatenate in 'f' its given CSVpath
@@ -23,19 +22,24 @@ class removeSpecifiedCols:
                 tail = head_tail[1]                 
                 # name of the file whithout the ext (i.e. from 'data.csv', splits (data, csv), then take the name (data))
                 filenameNoExt = tail.split(".")[0]
-                # propose new path and name for the file
-                head_tail1 = 'C:/Users/HP/Desktop/Remove_Columns_Python/Output' 
-                p = head_tail1 + "/" + filenameNoExt + " - Output.csv" 
-
-                mydata = pd.read_csv(f, encoding = 'latin1')
+                # p: propose new path and name for the file
+                save_path = self.saveCSVpath + "/" + filenameNoExt + " - Output.csv" 
+                return [f, save_path]
                 
-                new = mydata[["Field_1","Field_5","Field_30","Field_33","Field_40","Field_44","Field_61"]]
-                new.to_csv(p ,index=False)
+    def select_cols(self):
+        # create path
+        f, save_path = self.create_path(self, '.csv')
+        csv_data = pd.read_csv(f, encoding = 'latin1')
+        # selects needed columns and create new csv
+        new = csv_data[["Field_1","Field_5","Field_30","Field_33","Field_40","Field_44","Field_61"]]
+        new.to_csv(save_path, index = False)            
+                
 
 # run code
 # 1.- Open bash
-# 2.- Type "py removespecifiedCols.py [CSVpath]" where CSVpath is the path where CSV files are saved (can be found)                               
-
+# 2.- Type "py removespecifiedCols.py [findCSV] [saveCSV]" 
+# where [findCSV] is the path where CSV files can be found (place where saved)
+# and [saveCSV] is the path where new CSV files are going to be saved                          
 findCSV = 'C:\Users\I746992\OneDrive - SAP SE\General - Data Intelligence LAC\1. Mexico\2. Target List\3. Target Lists - Reports\7. Listados - 2024\Q2 - 2024\Rodrigo Lecanda'
 saveCSV = 'C:/Users/HP/Desktop/Remove_Columns_Python/Output' 
 remove = removeSpecifiedCols(findCSV, saveCSV)
