bb8b0c3a905edb26614ab48489ba96b05776f019
Martinez Ortiz <fabiola.martinez.ortiz@sap.com>
2024-02-22 10:37:30 -0600

    u: removeSpec code working - v2, check details pointed out on multiline coments | a: req.txt file

--- removeSpecifiedCols.py
+++ removeSpecifiedCols.py
@@ -6,37 +6,39 @@ import pandas as pd
 # Class: Removes specified columns from multiple CSV files 
 class removeSpecifiedCols:
 
+    def create_path(self, filename):
+        # splits current path into a tuple [head, tail]
+        #head_tail = os.path.split(f) 
+        # tail is the last pathname component a.k.a the name of the file (with ext)
+        #tail = head_tail[1]                 
+        # name of the file whithout the ext (i.e. from 'data.csv', splits (data, csv), then take the name (data))
+        filenameNoExt = filename.split(".")[0]
+        # p: propose new path and name for the file
+        save_path = str(self.saveCSVpath) + "/" + filenameNoExt + " - Output.csv" 
+        return save_path
+                
+    def select_cols(self):
+        # selects only .csv files from files in path
+        os.chdir(self.CSVpath)
+        list_csv_files = glob.glob('*.{}'.format('csv'))
+        """ Put try catch block for no existing files """
+        for filename in list_csv_files:
+            # create path
+            save_path = self.create_path(filename)
+            # Get complete path to access file
+            f = os.path.join(self.CSVpath, filename)
+            csv_read_data = pd.read_csv(f, encoding = 'latin1', usecols = ['User ID'])
+            csv_read_data.to_csv(save_path, index = False)   
+
+            #csv_data = pd.read_csv(f, encoding = 'latin1')
+            # selects needed columns and create new csv
+            #new = csv_data[["Field_1","Field_5","Field_30","Field_33","Field_40","Field_44","Field_61"]]
+            #new.to_csv(save_path, index = False)   
+
     # constructor
     def __init__(self, CSVpath, saveCSVpath):
         self.CSVpath = str(CSVpath)
-        self.saveCSVpath = saveCSVpath
-    
-    def create_path(self):
-        # selects only .csv files from files in path
-        os.chdir(self.saveCSVpath)
-        list_csv_files = glob.glob('*.{}'.format('.csv'))
-        for filename in list_csv_files:
-            # Get complete path to access file
-            f = os.path.join(self.CSVpath, filename)
-            # splits current path into a tuple [head, tail]
-            #head_tail = os.path.split(f) 
-            # tail is the last pathname component a.k.a the name of the file (with ext)
-            #tail = head_tail[1]                 
-            # name of the file whithout the ext (i.e. from 'data.csv', splits (data, csv), then take the name (data))
-            filenameNoExt = filename.split(".")[0]
-            # p: propose new path and name for the file
-            save_path = str(self.saveCSVpath) + "/" + filenameNoExt + " - Output.csv" 
-            return [f, save_path] 
-                
-    def select_cols(self):
-        # create path
-        f, save_path = self.create_path()
-        csv_read_data = pd.read_csv(f, encoding = 'latin1', usecols = ['ID', 'INDUSTRY (see Industry List)'])
-        csv_read_data.to_csv(save_path, index = False)        
-        #csv_data = pd.read_csv(f, encoding = 'latin1')
-        # selects needed columns and create new csv
-        #new = csv_data[["Field_1","Field_5","Field_30","Field_33","Field_40","Field_44","Field_61"]]
-        #new.to_csv(save_path, index = False)            
+        self.saveCSVpath = str(saveCSVpath)         
                 
 
 # run code
