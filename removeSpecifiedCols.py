532a7e02e7be206269579a426a69fee265db1590
Martinez Ortiz <fabiola.martinez.ortiz@sap.com>
2024-07-17 11:28:46 -0600

    updt: clearer2

--- removeSpecifiedCols.py
+++ removeSpecifiedCols.py
@@ -33,6 +33,5 @@ class removeSpecifiedCols:
 findCSV = 'C:/Users/I746992/OneDrive - SAP SE/General - Data Intelligence LAC/1. Mexico/2. Target List/3. Target Lists - Reports/7. Listados - 2024/Q2 - 2024/Rodrigo Lecanda'
 saveCSV = 'C:/Users/I746992/Desktop/Remove_Columns_Python/Output' 
 remove = removeSpecifiedCols(findCSV, saveCSV)
-#remove = removeSpecifiedCols(sys.argv[1], sys.argv[2]) 
 remove.select_cols()
 
